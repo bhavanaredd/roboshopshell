ccc3c2b4b5439e380305d1aa88c07251091b9960
bhavanap83 <bhavana220103@gmail.com>
2025-04-06 15:48:22 +0530

    Shell project commit

---
+++ mongodb.sh
@@ -0,0 +1,63 @@
+#!bin/bash
+
+id=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log" ### $0 denotes the script name 
+
+if [ $id -ne 0]
+then
+    echo "  $R Not Running as root user"
+    exit 1
+else 
+     echo "$G Running as a root user"
+fi
+
+Validate(){
+
+    if [$1 -ne 0]
+then
+    echo -e " $2  $R Not installed $N properly"
+else 
+    echo -e " $2 $G Installed $N properly"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> LOGFILE
+
+VALIDATE $? "Copied MongoDB Repo"
+
+dnf install mongodb-org -y  &>> LOGFILE
+
+VALIDATE $? "Installed Mongodb"
+
+systemctl enable mongod 
+
+VALIDATE $? "Enabled mongodb"
+
+systemctl start mongod 
+
+VALIDATE $? "started mongodb"
+
+cp /etc/mongod.conf /etc/mongod.conf.bak
+
+VALIDATE $? "Copied backup"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
+
+VALIDATE $? "changed listener adress"
+
+systemctl restart mongod
+
+VALIDATE $? "Restarted mongodb"
+
+
+
+
+
+
+
